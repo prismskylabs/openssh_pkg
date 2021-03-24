5680dc7eb5e052dc5577c9fbd7fb7fdb33010033
Yuriy Musatenko <yuriy@prismskylabs.com>
2021-03-24 13:47:18 -0700

    Fix for linux-amd64 build. Making sure conan is logged in

--- build-linux-amd64-oss.sh
+++ build-linux-amd64-oss.sh
@@ -8,6 +8,13 @@ TEST_BUILD_DIR=$ROOT_DIR/build
 
 [ -d $TEST_BUILD_DIR ] && rm -Rf $TEST_BUILD_DIR/* || mkdir -p $TEST_BUILD_DIR
 conan remote list
+CONAN_USER_INFO=$(conan user -r psl-conan)
+if [ "$CONAN_USER_INFO" == "Current user of remote 'psl-conan' set to: 'None' (anonymous)" ]; then
+    echo "Logging into psl-conan server."
+    echo "Please, enter psl-conan (artifactory-cpp) credentials when prompted"
+    conan user -r psl-conan  -p
+fi
+
 CONAN_PROF=$ROOT_DIR/src/external/edge_build_bootstrap/src/platforms/x86/linux/linux-x86_64-release.pr
-conan create $ROOT_DIR   -tbf $TEST_BUILD_DIR --profile $CONAN_PROF 
+conan create $ROOT_DIR   -tbf $TEST_BUILD_DIR --profile $CONAN_PROF
 
